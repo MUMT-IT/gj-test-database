b38786d07662241c2ad79689ee1a2a0ef89aa298
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-12-28 11:17:43 +0700

    Added user model.

--- app/gj_test/models.py
+++ app/gj_test/models.py
@@ -82,9 +82,9 @@ class GJTestWaitingPeriod(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True)
-    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(), unique=True, index=True)
+    username = db.Column(db.String(), unique=True)
+    password_hash = db.Column(db.String())
 
     def __repr__(self):
         return '<User({username!r})>'.format(username=self.username)
